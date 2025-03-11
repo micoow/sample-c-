b94f4356b20193628a8fc5e7e43144624b13c887
unknown <mico.bustillo775@gmail.com>
2025-03-11 22:51:19 +0800

    master

---
+++ carlos_paldo.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+
+using namespace std;
+
+void findSum(int a,int b)
+{
+	cout<<"Enter the first number: ";
+	cin>>a;
+	cout<<"Enter the second number: ";
+	cin>>b;
+	int sum=a+b;
+	
+}
+
+int main()
+{
+	int x,y;
+	findSum(x,y);
+	return 0;
+}
